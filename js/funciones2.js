0b4576c5875ee9e37d015c713c70086694722f91
Aleamoca <aleamoca@gmail.com>
2023-10-28 17:38:09 +0200

    Subir Tarea

--- js/funciones2.js
+++ js/funciones2.js
@@ -1,65 +1,82 @@
-//Funciones
-document.getElementById("procesarPedidoButton").addEventListener("click", function() {
-    calcularPrecio();
+document.addEventListener('DOMContentLoaded', function () {
+    // Obtener el contenedor
+    const formularioPedido = document.getElementById('formulario-pedido');
+
+    // Crear los elementos del formulario
+    
+    const nombreInput = createInput('Nombre', 'text');
+    const direccionInput = createInput('Dirección', 'text');
+    const telefonoInput = createInput('Teléfono', 'number');
+    const emailInput = createInput('Email', 'text');
+
+    // Radio buttons
+    const radioButtons = createRadioButtons(['Pequeña', 'Mediana', 'Grande']);
+
+    // Checkboxes
+    const checkboxes = createCheckboxes([
+        'Jamón',
+        'Queso',
+        'Tomate',
+        'Aceitunas'
+    ]);
+
+     // Añadir los elementos al contenedor
+     formularioPedido.appendChild(nombreInput);
+     formularioPedido.appendChild(direccionInput);
+     formularioPedido.appendChild(telefonoInput);
+     formularioPedido.appendChild(emailInput);
+     formularioPedido.appendChild(radioButtons);
+     formularioPedido.appendChild(checkboxes);
 });
-function calcularPrecio() {
-    // Obtener los valores de los elementos del formulario
-    var tamano = document.querySelector('input[name="tamano"]:checked');
-    var ingredientes = document.querySelectorAll('input[type="checkbox"]:checked');
 
+// Funciones   
 
-    // Verificar que se haya seleccionado al menos un tamaño y un ingrediente
-    if (!tamano || ingredientes.length === 0) {
-        alert("Por favor, elija al menos un tamaño y un ingrediente.");
-        return;
-    }
+function createInput(labelText, inputType) {
+    const label = document.createElement('label');
+    label.textContent = labelText;
 
-    // Calcular el precio
-    var precioBase;
-    switch (tamano.value) {
-        case "pequena":
-            precioBase = 5;
-            break;
-        case "mediana":
-            precioBase = 10;
-            break;
-        case "grande":
-            precioBase = 15;
-            break;
-    }
+    const input = document.createElement('input');
+    input.type = inputType;
 
-    var precioIngredientes = ingredientes.length; // Cada ingrediente tiene un coste de 1€ +
+    const pedido = document.createElement('div');
+    pedido.appendChild(label);
+    pedido.appendChild(input);
 
-    var precioTotal = precioBase + precioIngredientes;
-
-    // Mostrar el resultado
-    var resultadoDiv = document.getElementById("resultado");
-    resultadoDiv.innerHTML = "Precio Total del Pedido: " + precioTotal + "€";
+    return pedido;
 }
 
+function createRadioButtons(options) {
+    const pedido = document.createElement('div');
 
-document.addEventListener('DOMContentLoaded', function () {
-    var botonProcesar = document.getElementById('procesarPedido');
-    botonProcesar.addEventListener('click', function () {
-        calcularPrecio();
+    options.forEach(option => {
+        const label = document.createElement('label');
+        label.textContent = option;
+
+        const radio = document.createElement('input');
+        radio.type = 'radio';
+        radio.name = 'radio-group';
+
+        contenedor.appendChild(label);
+        contenedor.appendChild(radio);
     });
-});
 
-function calcularPrecio() {
-    var tamano = document.querySelector('input[name="tamano"]:checked');
-    var ingredientes = document.querySelectorAll('input[type="checkbox"]:checked');
+    return pedido;
+}
 
-    if (tamano === null || ingredientes.length === 0) {
-        alert("Debe seleccionar tamaño y al menos un ingrediente.");
-        return;
-    }
+function createCheckboxes(options) {
+    const pedido = document.createElement('div');
 
-    var precioBase = parseInt(tamano.value);
-    var precioIngredientes = Array.from(ingredientes).reduce(function (total, ingrediente) {
-        return total + parseInt(ingrediente.value);
-    }, 0);
+    options.forEach(option => {
+        const label = document.createElement('label');
+        label.textContent = option;
 
-    var precioTotal = precioBase + precioIngredientes;
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
 
-    var resultadoDiv = document.getElementById("resultado");
-    resultadoDiv.innerHTML = "Precio total del pedido: " + precioTotal + "€";}
+        pedido.appendChild(label);
+        pedido.appendChild(checkbox);
+    });
+
+    return pedido;
+}
+
