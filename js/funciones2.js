07128f8783bdaee3fb1199c7807a4a54bbfbb381
Aleamoca <aleamoca@gmail.com>
2023-10-28 17:12:18 +0200

    Subir Tarea

--- js/funciones2.js
+++ js/funciones2.js
@@ -1,90 +1,4 @@
-/*
-document.addEventListener('DOMContentLoaded', function () {
-    // Obtener el contenedor
-    const formularioPedido = document.getElementById('formulario-pedido');
-
-    // Crear los elementos del formulario
-    
-    const nombreInput = createInput('Nombre', 'text');
-    const direccionInput = createInput('Dirección', 'text');
-    const telefonoInput = createInput('Teléfono', 'number');
-    const emailInput = createInput('Email', 'text');
-
-    // Radio buttons
-    const radioButtons = createRadioButtons(['Pequeña', 'Mediana', 'Grande']);
-
-    // Checkboxes
-    const checkboxes = createCheckboxes([
-        'Jamón',
-        'Queso',
-        'Tomate',
-        'Aceitunas'
-    ]);
-
-     // Añadir los elementos al contenedor
-     formularioPedido.appendChild(nombreInput);
-     formularioPedido.appendChild(direccionInput);
-     formularioPedido.appendChild(telefonoInput);
-     formularioPedido.appendChild(emailInput);
-     formularioPedido.appendChild(radioButtons);
-     formularioPedido.appendChild(checkboxes);
-});
-
-// Funciones   
-
-function createInput(labelText, inputType) {
-    const label = document.createElement('label');
-    label.textContent = labelText;
-
-    const input = document.createElement('input');
-    input.type = inputType;
-
-    const pedido = document.createElement('div');
-    pedido.appendChild(label);
-    pedido.appendChild(input);
-
-    return pedido;
-}
-
-function createRadioButtons(options) {
-    const pedido = document.createElement('div');
-
-    options.forEach(option => {
-        const label = document.createElement('label');
-        label.textContent = option;
-
-        const radio = document.createElement('input');
-        radio.type = 'radio';
-        radio.name = 'radio-group';
-
-        contenedor.appendChild(label);
-        contenedor.appendChild(radio);
-    });
-
-    return pedido;
-}
-
-function createCheckboxes(options) {
-    const pedido = document.createElement('div');
-
-    options.forEach(option => {
-        const label = document.createElement('label');
-        label.textContent = option;
-
-        const checkbox = document.createElement('input');
-        checkbox.type = 'checkbox';
-
-        pedido.appendChild(label);
-        pedido.appendChild(checkbox);
-    });
-
-    return pedido;
-}
-
-
-*/
 //Funciones
-
 document.getElementById("procesarPedidoButton").addEventListener("click", function() {
     calcularPrecio();
 });
@@ -114,7 +28,7 @@ function calcularPrecio() {
             break;
     }
 
-    var precioIngredientes = ingredientes.length; // Cada ingrediente tiene un costo de 1€
+    var precioIngredientes = ingredientes.length; // Cada ingrediente tiene un coste de 1€ +
 
     var precioTotal = precioBase + precioIngredientes;
 
@@ -123,7 +37,6 @@ function calcularPrecio() {
     resultadoDiv.innerHTML = "Precio Total del Pedido: " + precioTotal + "€";
 }
 
-/*
 
 document.addEventListener('DOMContentLoaded', function () {
     var botonProcesar = document.getElementById('procesarPedido');
@@ -149,4 +62,4 @@ function calcularPrecio() {
     var precioTotal = precioBase + precioIngredientes;
 
     var resultadoDiv = document.getElementById("resultado");
-    resultadoDiv.innerHTML = "Precio total del pedido: " + precioTotal + "€";}*/
+    resultadoDiv.innerHTML = "Precio total del pedido: " + precioTotal + "€";}
